5dcce1b00f0d56aabd9809291062c3a807213c6a
“Richard Middleton” <Richard@richardmiddleton.me>
2017-02-19 18:52:53 +0100

    created updates to maths

--- main.js
+++ main.js
@@ -1,50 +1,55 @@
 (function() {
 
+	// pull value from text field and set to object
 
-  // pull value from text field and set to object
+	document.getElementById("my-btn").addEventListener("click", function() {
+		book.name = document.getElementById('booktitle').value;
+		bookName.textContent = book.name;
 
-  document.getElementById("my-btn").addEventListener("click", function() {
-      book.name = document.getElementById('booktitle').value;
-      bookName.textContent = book.name;
+		book.totalPages = document.getElementById('total-pages').value;
+		totalPages.textContent = 'Total Pages: ' + book.totalPages;
 
-      book.totalPages = document.getElementById('total-pages').value;
-      totalPages.textContent = 'Total Pages: ' + book.totalPages;
+		book.pages = document.getElementById('pages').value;
+		book.pages.textContent = book.pages;
 
-      book.pages = document.getElementById('pages').value;
-      book.pages.textContent = book.pages;
-  });
+		pagesLeft = document.getElementById('pagesLeft');
+		pagesLeft.textContent = book.pagesLeft();
 
-// Create book object
+		percentageLeft = document.getElementById('percentageLeft');
+		percentageLeft.textContent = book.percentageLeft() + '%';
 
-var book =  {
+	});
 
-name: 'Tom & Jerry',
-totalPages: 622,
-pages: 162,
-pagesLeft: function() {
-  var total = this.totalPages - this.pages;
-  return total;
-},
-percentageLeft: function() {
-  var totalPercentage = this.pagesLeft()/this.totalPages * 100
-  return Math.round(totalPercentage);
-}
-};
+	// Create book object
 
+	var book = {
 
-// write out book name and pages info
+		name: 'Tom & Jerry',
+		totalPages: 622,
+		pages: 162,
+		pagesLeft: function() {
+			var total = this.totalPages - this.pages;
+			return total;
+		},
+		percentageLeft: function() {
+			var totalPercentage = this.pagesLeft() / this.totalPages * 100
+			return Math.round(totalPercentage);
+		}
+	};
 
-var bookName, totalPages, pagesLeft, percentageLeft; //declares variables
+	// write out book name and pages info
 
-bookName = document.getElementById('bookName'); // gets elements from document
-totalPages = document.getElementById('totalPages');
-pagesLeft = document.getElementById('pagesLeft');
-percentageLeft = document.getElementById('percentageLeft');
+	var bookName, totalPages, pagesLeft, percentageLeft; //declares variables
 
+	bookName = document.getElementById('bookName'); // gets elements from document
+	totalPages = document.getElementById('totalPages');
 
-bookName.textContent = book.name; // write to document
-totalPages.textContent = 'Total Pages: ' + book.totalPages;
-pagesLeft.textContent =  book.pagesLeft();
-percentageLeft.textContent =  book.percentageLeft() + '%';
+	pagesLeft = document.getElementById('pagesLeft');
+	percentageLeft = document.getElementById('percentageLeft');
+
+	bookName.textContent = book.name; // write to document
+	totalPages.textContent = 'Total Pages: ' + book.totalPages;
+	pagesLeft.textContent = book.pagesLeft();
+	percentageLeft.textContent = book.percentageLeft() + '%';
 
 }());
